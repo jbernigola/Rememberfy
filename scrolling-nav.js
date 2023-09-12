bc74c1e7dc41398b32e3ec99acc98f2c0c05365c
Juan I. Bernigola <juannybernigola@gmail.com>
2023-09-12 19:19:12 -0300

    Solucion parcial: poner Footer dentro del Main

---
+++ scrolling-nav.js
@@ -0,0 +1,9 @@
+// Escucha el evento scroll y agrega la clase "scrolled" cuando se desplaza hacia abajo
+window.addEventListener("scroll", function() {
+    const header = document.querySelector(".header");
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
