2687b17cb0bba6be5c3222084e5a1acef72824f6
Juan I. Bernigola <juannybernigola@gmail.com>
2023-09-13 00:52:33 -0300

    Landing Page Style: The 'Banner-Nav'

--- scrolling-nav.js
+++ scrolling-nav.js
@@ -1,7 +1,7 @@
 // Escucha el evento scroll y agrega la clase "scrolled" cuando se desplaza hacia abajo
 window.addEventListener("scroll", function() {
     const header = document.querySelector(".header");
-    if (window.scrollY > 0) {
+    if (window.scrollY > 75) {
         header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
